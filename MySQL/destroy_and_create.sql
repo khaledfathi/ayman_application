ecd495da8552db8dc024f723e67672a2c1783cce
Khaled Fathi <khaledfathi@protonmail.com>
2018-12-20 17:44:07 +0200

    Add files via upload

---
+++ MySQL/destroy_and_create.sql
@@ -0,0 +1,19 @@
+DROP TABLE  IF EXISTS  `ayman`.`orders`;
+
+CREATE TABLE `ayman`.`orders`(
+`id` INT(6) AUTO_INCREMENT PRIMARY KEY ,
+`name` VARCHAR(50) CHARSET utf8,
+`address` VARCHAR(50) CHARSET utf8,
+`phone` VARCHAR(50) CHARSET utf8,
+`cities` VARCHAR(50) CHARSET utf8 ,
+`product_type`VARCHAR(255) CHARSET utf8,
+`product_link`TEXT CHARSET utf8,
+`pic_file` TEXT CHARSET utf8,
+`pic_url` TEXT CHARSET utf8,
+`request_date` DATE,
+`end_date` DATE ,
+`email`	VARCHAR(50) CHARSET utf8,
+`souq_password` VARCHAR(50) CHARSET utf8,\
+`request_status` VARCHAR(50) CHARSET utf8,
+`notes` TEXT CHARSET utf8
+);
